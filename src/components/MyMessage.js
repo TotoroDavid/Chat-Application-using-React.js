86f9957cf4f6f2052da005db5733554f6b4ca2bb
TotoroDavid <davidtoro65@hotmail.com>
2021-03-25 18:07:58 -0500

    fixed myMessage and placefolder completed

--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,28 +1,28 @@
-const MyMessage = (message) => {
+const MyMessage = ({message}) => {
 
-    if(message?.length > 0) {
+    if (message ?. attachments ?. length > 0) {
         return (
-            <img 
-                src={message.attachments[0].file}
-                alt='message-attachment'
-                className='message-image'
-                style={{float : 'right'}}
+            <img src={message.attachments[0].file} 
+            alt="message-attachment"
+            className="message-image"    
+            style = {{float: 'right'}}
             />
         )
     }
 
     return (
         <div 
-        className='message' 
+        className="message" 
         style=
-            {{ float : 'right',
-            marginRight : '18px',
-            color : 'white',
-            backgroundColor : '#3B2A50' }}
+            {{float: 'right', 
+            marginRight: '18px',
+            color: 'white',
+            backgroundColor: '#3B2A50'}}
         >
-            {message.text}    
+            {message.text}
         </div>
     )
+
 }
 
 export default MyMessage
